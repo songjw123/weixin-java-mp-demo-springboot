337c49f7165da9d0aa5f3b89e264c098cf7c0bc2
Binary Wang <binarywang@gmail.com>
2017-03-06 22:38:19 +0800

    fix something

--- src/main/java/com/github/binarywang/demo/wechat/controller/WechatController.java
+++ src/main/java/com/github/binarywang/demo/wechat/controller/WechatController.java
@@ -4,12 +4,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import me.chanjar.weixin.mp.api.WxMpMessageRouter;
 import me.chanjar.weixin.mp.api.WxMpService;
